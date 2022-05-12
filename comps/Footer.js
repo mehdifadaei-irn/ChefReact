3f18a4f35177c91ce0ba11d51c05d920d6660269
mehdi fadaei <m_mehdi_000@yahoo.com>
2022-05-12 13:19:07 +0430

    ez header footer

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
+import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
 
 
-const Footer = ()=> {
+const Footer = () => {
     return (
-        <div>Hero Banner</div>
+        <div className={'footer-container'}>
+            <p>2021 mehdi Headphones All here</p>
+            <p>
+                <AiFillInstagram color={'green'} fontSize={40} style={{cursor: 'pointer'}}/>
+                <AiOutlineTwitter color={'green'} fontSize={40} style={{cursor: 'pointer'}}/>
+            </p>
+
+        </div>
     )
 }
 
